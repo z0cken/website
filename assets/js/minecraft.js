746963e2b5e5167dfd88d87748083d136492caa0
Julian Einwager <j.einwanger@googlemail.com>
2020-07-05 15:33:46 +0200

    Made into Jekyll project

--- assets/js/minecraft.js
+++ assets/js/minecraft.js
@@ -7,9 +7,3 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.pushpin');
   var instances = M.Pushpin.init(elems, {offset: 150});
 });
-
-// Initialize collapsible (uncomment the lines below if you use the dropdown variation)
-var collapsibleElem = document.querySelector('.collapsible');
-var collapsibleInstance = M.Collapsible.init(collapsibleElem);
-
-M.AutoInit();
