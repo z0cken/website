211fcf00faa1f00d12a71e175fba50ce2384ed2f
Julian Einwanger <j.einwanger@googlemail.com>
2020-07-07 17:34:47 +0200

    Oauth preparation

--- assets/js/verify.js
+++ assets/js/verify.js
@@ -1,15 +1,16 @@
+var botDomain = "http://localhost:8080";
+var proClientID = 456;
+
 var discordLink =
   "https://discord.com/api/oauth2/authorize?client_id=703946237291790356&redirect_uri=https%3A%2F%2Fz0cken.github.io%2Fwebsite%2Fverification%2F&response_type=token&scope=identify&state=#";
-var programmLink = "https://pr0gramm.com/auth?clientId=1234&state=#";
-
-var botDomain = 'http://localhost:8080';
-var proClientID;
+var programmLink = "https://pr0gramm.com/auth/z0cken/#?clientID=" + proClientID;
 
 var discordToken;
 var discordTokenType;
 
 var proAuthCode;
 var proUserID;
+var proName = "Unknown"
 
 window.onload = function () {
   var url_string = window.location.href.replace("#", "?");
@@ -17,12 +18,11 @@ window.onload = function () {
   discordToken = url.searchParams.get("access_token");
   discordTokenType = url.searchParams.get("token_type");
   proAuthCode = url.searchParams.get("authCode");
-  proUserID = url.searchParams.get("userID");
+  proUserID = url.searchParams.get("userId");
   setStates(url.searchParams.get("state"));
   //Done extracting URL params
   window.history.pushState({}, "z0cken Verify", window.location.pathname);
   setButtons();
-  document.getElementById("request-button").addEventListener("click",sendRequest)
 };
 
 function setButtons() {
@@ -44,7 +44,7 @@ function setButtons() {
   }
 
   if (proAuthCode && proUserID && discordToken) {
-    //Send to Bot
+    sendRequest()
   }
   document.getElementById("dc-button").href = discordLink.replace(
     "#",
@@ -65,16 +65,16 @@ function setStates(string) {
 
 function setState(value, index, array) {
   var pairs = value.split("_");
-  if (pairs[0].equals("dct") && !discordToken) {
+  if (pairs[0] === "dct" && !discordToken) {
     discordToken = pairs[1];
   }
-  if (pairs[0].equals("dctt") && !discordTokenType) {
+  if (pairs[0] === "dctt" && !discordTokenType) {
     discordTokenType = pairs[1];
   }
-  if (pairs[0].equals("pac") && !proAuthCode) {
+  if (pairs[0] === "pac" && !proAuthCode) {
     proAuthCode = pairs[1];
   }
-  if (pairs[0].equals("puid") && !proUserID) {
+  if (pairs[0] === "puid" && !proUserID) {
     proUserID = pairs[1];
   }
 }
@@ -91,7 +91,10 @@ function getState() {
     state_string += "pac_" + proAuthCode + "-";
   }
   if (proUserID) {
-    state_string += "puid-" + proUserID + "-";
+    state_string += "puid_" + proUserID + "-";
+  }
+  if (state_string.length == 0) {
+    state_string = "no_state"
   }
   return state_string;
 }
@@ -115,45 +118,59 @@ function setDiscordName() {
 }
 
 function setProgrammName() {
-  document.getElementById("proButton").innerHTML = "Verifiziert als " + proName;
+      document.getElementById("pro-button").innerHTML = "Verifiziert";
+      document.getElementById("pro-text").innerHTML =
+        "Registriert als " + proUserID + ".";
 }
 
 function sendRequest() {
+
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems, {dismissible: false, endingTop: "33%"});
+  instances[0].open();
+
+  
   var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
+  xhr.onreadystatechange = function () {
     if (xhr.readyState == XMLHttpRequest.DONE) {
-      document.getElementById("request-button").innerHTML = xhr.responseText
+      document.getElementById("modal-loading-content").classList.add("hide");
+      document.getElementById("modal-recieved-content").classList.remove("hide");
+      if (xhr.status === 200) {
+        messages = xhr.responseText.split(":")
+        document.getElementById("modal-response-header").innerHTML = messages[0]
+        document.getElementById("modal-response-text").innerHTML = messages[1]
+
+      } else {
+        document.getElementById("modal-response-header").innerHTML = "Fehler"
+        document.getElementById("modal-response-text").innerHTML =  "Bot nicht erreichbar. <br>Bitte versuche es erneut oder kontaktiere uns im Discord!"
+      }
     }
-  }
+  };
   xhr.open("POST", botDomain, true);
   xhr.send(
     JSON.stringify({
-      "discordTokenType": discordTokenType,
-      "discordToken": discordToken,
-      "proAuthCode": proAuthCode,
-      "proUserID": proUserID
+      discordTokenType: discordTokenType,
+      discordToken: discordToken,
+      proAuthCode: proAuthCode,
+      proUserID: proUserID,
     })
   );
 }
 
-
 var open = false;
-unloadcooldown = function(e) {
+unloadcooldown = function (e) {
   open = true;
   setTimeout(() => {
     open = false;
   }, 1);
-}
-
-document.getElementById("dc-button").addEventListener("click", unloadcooldown)
-document.getElementById("pro-button").addEventListener("click", unloadcooldown)
-
-
-window.onbeforeunload = function(e) {
-  if (!this.open) {
-    console.log(e)
-    e.preventDefault()
-    return 'ACHTUNG: Danach ist dein Fortschritt verloren.';
-  }
 };
 
+document.getElementById("dc-button").addEventListener("click", unloadcooldown);
+document.getElementById("pro-button").addEventListener("click", unloadcooldown);
+
+window.onbeforeunload = function (e) {
+  if (!this.open) {
+    e.preventDefault();
+    return "ACHTUNG: Danach ist dein Fortschritt verloren.";
+  }
+};
